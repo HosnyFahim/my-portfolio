329a07d1f38d65b71009113f8033816faedb3b73
HosnyFahim <hosny.fahim1990@gmail.com>
2023-05-24 19:31:24 +0200

    header, navbar and logo added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-portfolio');
+    expect(app).toEqual('my-portfolio');
   });
 
   it('should render title', () => {
