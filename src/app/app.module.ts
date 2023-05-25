bb0ce5ef377f01688242fd7aa3f04fab285d554d
HosnyFahim <hosny.fahim1990@gmail.com>
2023-05-26 01:25:01 +0200

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { ConctactComponent } from './conctact/conctact.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    MyWorkComponent,
+    ConctactComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/conctact/conctact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conctact',
+  templateUrl: './conctact.component.html',
+  styleUrls: ['./conctact.component.scss']
+})
+export class ConctactComponent {
+
+}
