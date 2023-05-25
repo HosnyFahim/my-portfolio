2e6dcd660b5c37c4d57c85467a2d9ebdc07367b7
HosnyFahim <hosny.fahim1990@gmail.com>
2023-05-26 00:46:05 +0200

    home animation added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  words: string[] = ['Web Designer', 'Frontend Developer'];
+  currentWordIndex: number = 0;
+  currentWord: string = '';
+  isTyping: boolean = false;
 
+  ngOnInit() {
+    this.animateWords();
+  }
+
+  animateWords() {
+    setInterval(() => {
+      if (!this.isTyping) {
+        this.typeWord(this.words[this.currentWordIndex])
+          .then(() => this.deleteWord(this.words[this.currentWordIndex]))
+          .then(() => {
+            this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+          });
+      }
+    }, 2000);
+  }
+
+  typeWord(word: string): Promise<void> {
+    this.isTyping = true;
+    return new Promise<void>((resolve) => {
+      let i = 0;
+      const intervalId = setInterval(() => {
+        this.currentWord = word.slice(0, i);
+        i++;
+        if (i > word.length) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }, 200);
+    });
+  }
+
+  deleteWord(word: string): Promise<void> {
+    return new Promise<void>((resolve) => {
+      let i = word.length;
+      const intervalId = setInterval(() => {
+        this.currentWord = word.slice(0, i);
+        i--;
+        if (i < 0) {
+          clearInterval(intervalId);
+          resolve();
+          this.isTyping = false;
+        }
+      }, 100);
+    });
+  }
 }
