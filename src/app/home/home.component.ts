953bbe525da78b05bf627321f7115064ed719b9d
HosnyFahim <hosny.fahim1990@gmail.com>
2023-06-02 22:40:01 +0200

    portfolio section and imgs added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  words: string[] = ['Software engineer', 'Frontend Developer'];
+  words: string[] = ['Programmer', 'Frontend Developer'];
   currentWordIndex: number = 0;
   currentWord: string = '';
   isTyping: boolean = false;
