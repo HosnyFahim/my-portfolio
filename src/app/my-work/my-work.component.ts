5ab6661e88d3a15ea9241180bd1de2fcb12ff609
HosnyFahim <hosny.fahim1990@gmail.com>
2023-06-06 21:42:01 +0200

    my work component funktions added

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-work.component.scss']
 })
 export class MyWorkComponent {
+  projects = [
+    {
+      title: 'Join',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      image: '/assets/img/join.png',
+      category: 'javascript'
+    },
+    {
+      title: 'El Pollo Loco',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      image: '/assets/img/pollo-loco.png',
+      category: 'angular'
+    },
+    {
+      title: 'Ring of Fire',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      image: '/assets/img/join.png',
+      category: 'angular'
+    },
+    {
+      title: 'Portfolio',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      image: '/assets/img/pollo-loco.png',
+      category: 'javascript'
+    }
+  ];
 
+  activeButton = 'all';
+
+  get filteredProjects() {
+    if (this.activeButton === 'angular') {
+      return this.projects.filter(project => project.category === 'angular').slice(-2);
+    } else if (this.activeButton === 'javascript') {
+      return this.projects.filter(project => project.category === 'javascript').slice(0, 2);
+    } else {
+      return this.projects;
+    }
+  }
+
+  setActiveButton(category: string) {
+    this.activeButton = category;
+  }
 }
