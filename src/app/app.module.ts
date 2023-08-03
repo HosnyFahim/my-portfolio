39b56c726bf9726d7402d5bb5de184c4f17c1b1d
HosnyFahim <hosny.fahim1990@gmail.com>
2023-08-03 20:16:33 +0200

    send mail backend added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -35,7 +35,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    FormsModule, 
+    FormsModule,
+    ReactiveFormsModule, 
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/conctact/conctact.component.ts
+++ src/app/conctact/conctact.component.ts
@@ -1,11 +1,20 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-conctact',
   templateUrl: './conctact.component.html',
   styleUrls: ['./conctact.component.scss']
 })
-export class ConctactComponent {
+export class ConctactComponent implements OnInit {
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('mailField') mailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
   activeInput: string | null = ''; // Variable zum Speichern des aktiven Eingabefelds
   nameInputValue: string = ''; // Variable zum Speichern des Wertes des "Your Name" Input-Felds
   emailInputValue: string = ''; // Variable zum Speichern des Wertes des "Your Email" Input-Felds
@@ -39,5 +48,38 @@ export class ConctactComponent {
     if (!target.closest('input') && !target.closest('textarea')) {
       this.activeInput = null;
     }
+  }
+
+  async sendMail() {
+    console.log('Sending mail', this.myForm);
+    let nameField = this.nameField.nativeElement;
+    let mailField = this.mailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+
+    nameField.disabled = true;
+    mailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', mailField.value);
+    fd.append('message', messageField.value);
+ 
+    //senden
+
+    await fetch('https://hosny-fahim.developerakademie.net/send_mail/send_mail.php', 
+      {
+        method: 'POST',
+        body: fd
+      }
+    );
+
+    // nameField.disabled = false;
+    // mailField.disabled = false;
+    // messageField.disabled = false;
+    // sendButton.disabled = false;
+
   }
 }
